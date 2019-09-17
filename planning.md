6ce7bcf31a2d573d18bef9a0f51faa83a1fac00c
Andrew <drew.chappetta@gmail.com>
2019-09-17 15:11:17 -0500

    Built board

--- planning.md
+++ planning.md
@@ -22,14 +22,27 @@ Hint to acomplish
 
 ### Actual Movement
 I should be able to give a piece a "movement value".
-  - Ex: (3, -1) which would be three block forward and one block to the left
+  - Ex: (-1,3) which would be three block forward and one block to the left
 
 Im not 100% how im going to implement a piece not being able to move thru another piece but im thinking that when a piece moves it does just appear on that tile. Instead it would check each step along the way. 
-  - Ex: a bishop moving that wants to move (3,-3) would first check the tile at (1,-1) then (2,-2) then (3, -3) if there is a piece at (2,-2) it should not be able to move to that square.
+  - Ex: a bishop moving that wants to move (-3,3) would first check the tile at (-1,1) then (-2,2) then (-3, 3) if there is a piece at (-2,2) it should not be able to move to that square.
 
 ## Piece Properties 
 * Pawn 
-* * Moves
-
+ * Moves up to two squares on first move then one after that
+ * Can attack diagonally
+ * Special moves: En Passant & promotion 
+* Rook
+ * Moves any number of spaces vertically or horizontally
+ * Special moves: Castling
+* Knight 
+ * Moves in L shape (2,-1) (2,1) (-1,2) (1,2) (-2,1) (-2,-1) (1,-2) (-1,-2)
+ * Can jump over enemies
+* Bishop 
+ * can move diagonal in any direction. (1,1) (2,2) ...
+* Queen 
+ * can move vertically, horizontal, diagonally any number of spaces
+* King 
+ * can move vertically, horizontal, diagonally one space
 
 (This might be usefule for styling: https://www.chess.com/article/view/chess-board-dimensions)
--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,6 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
-import Array
+import Array exposing (Array)
 import Browser
 import List exposing (range)
 import Html exposing (Html, button, div, h1, h3, text, h4)
@@ -18,28 +18,115 @@ type alias Document msg =
   , body : List (Html msg)
   }
 
-type alias Model =
-  {}
-
 type Player 
-  = PlayerB 
-  | PlayerW
+  = Black 
+  | White
+
+type alias GameBoard 
+  = Array Square
+
+type Square
+  = Occupied Unit
+  | Unoccupied
+
+type Unit
+  = Pawn Player
+  | Rook Player
+  | Knight Player
+  | Bishop Player
+  | Queen Player
+  | King Player
+
+type alias Model =
+  { board : GameBoard
+  , currentPlayer : Player
+  , activePiece : Maybe Unit
+  }
 
 type Msg
   = Reset
 
 
-
 init : () -> ( Model, Cmd Msg )
 init _ = 
-  ( {}, Cmd.none)
+  ( { board = defaultBoard
+    , currentPlayer = White
+    , activePiece = Nothing }, Cmd.none)
+
+
+defaultBoard: Array Square
+defaultBoard = 
+  Array.fromList [ Occupied (Rook Black)
+  , Occupied (Knight Black)
+  , Occupied (Bishop Black)
+  , Occupied (King Black)
+  , Occupied (Queen Black)
+  , Occupied (Bishop Black)
+  , Occupied (Knight Black)
+  , Occupied (Rook Black)
+  , Occupied (Pawn Black)
+  , Occupied (Pawn Black)
+  , Occupied (Pawn Black)
+  , Occupied (Pawn Black)
+  , Occupied (Pawn Black)
+  , Occupied (Pawn Black)
+  , Occupied (Pawn Black)
+  , Occupied (Pawn Black)
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Unoccupied
+  , Occupied (Pawn White)
+  , Occupied (Pawn White)
+  , Occupied (Pawn White)
+  , Occupied (Pawn White)
+  , Occupied (Pawn White)
+  , Occupied (Pawn White)
+  , Occupied (Pawn White)
+  , Occupied (Pawn White)
+  , Occupied (Rook White)
+  , Occupied (Knight White)
+  , Occupied (Bishop White)
+  , Occupied (Queen White)
+  , Occupied (King  White)
+  , Occupied (Bishop White)
+  , Occupied (Knight White)
+  , Occupied (Rook White) ]
 
 
 changePlayer : Player -> Player
 changePlayer player =
   case player of
-    PlayerB -> PlayerW
-    PlayerW -> PlayerB
+    Black -> White
+    White -> Black
 
 --Update
 
@@ -47,11 +134,80 @@ update : Msg -> Model -> ( Model, Cmd Msg)
 update msg model =
   case msg of
     Reset ->
-      ( model, Cmd.none)
+      ( 
+        { board = defaultBoard
+        , currentPlayer = White
+        , activePiece = Nothing
+      }, Cmd.none)
 
 --View
 
 view : Model -> Document Msg
 view model =
   { title = "Elm-Chess"
-  , body = [ h1 [] [ text "Elm Chess"] ] }
+  , body = 
+    [ h1 [] [ text "Elm Chess"] 
+      , div [class "game-content"] (range 1 8 |> List.map (viewBoardRow model.board)) 
+    ] }
+
+
+viewBoardRow : GameBoard -> Int -> Html Msg 
+viewBoardRow board index =
+  let 
+    rangeStart = (index - 1) * 8 
+    rangeEnd = index * 8 - 1 
+  in  
+    div [ class "row" ] (range rangeStart rangeEnd |> List.map (viewBoardButton board))
+
+
+viewBoardButton : GameBoard -> Int -> Html Msg 
+viewBoardButton board index =
+  div [ class ("square square-" ++ (String.fromInt index)) ] [ Array.get index board |> viewBoardButtonText |> text ]
+
+viewBoardButtonText : Maybe Square -> String
+viewBoardButtonText maybeSquare =
+  case maybeSquare of
+    Just squareValue ->
+      case squareValue of
+        Occupied unit ->
+          case unit of
+            Pawn player ->
+              case player of
+                Black ->
+                  "B-P"
+                White ->
+                  "W-P"
+            Rook player -> 
+              case player of
+                Black ->
+                  "B-R"
+                White ->
+                  "W-R"
+            Knight player ->
+              case player of
+                Black ->
+                  "B-K"
+                White ->
+                  "W-K"
+            Bishop player ->
+              case player of
+                Black ->
+                  "B-B"
+                White ->
+                  "W-B"
+            Queen player ->
+              case player of
+                Black ->
+                  "B-Q"
+                White ->
+                  "W-Q"
+            King player ->
+              case player of
+                Black ->
+                  "B-King"
+                White ->
+                  "W-King"
+        Unoccupied ->
+          "E"
+    Nothing ->
+      "E"
